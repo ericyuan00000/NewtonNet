f950854592552ae0bc82b1ab4da19335c78e0470
Eric Yuan <ericyuan@berkeley.edu>
2024-12-05 15:33:27 -0800

    Update guess neighbor count

--- newtonnet/data/neighbors.py
+++ newtonnet/data/neighbors.py
@@ -9,6 +9,11 @@ class RadiusGraph(BaseTransform):
 
     Args:
         r (float): The distance.
+        max_num_neighbors (int, optional): The maximum number of neighbors to
+            return for each element in :obj:`y`.
+            This flag is only needed for CUDA tensors. (default: :obj:`32`)
+            If the number of neighbors exceeds :obj:`max_num_neighbors`, this 
+            flag will be increased by a factor of :obj:`2` until it is sufficient.
         loop (bool, optional): If :obj:`True`, the graph will contain
             self-loops. (default: :obj:`False`)
         flow (str, optional): The flow direction when using in combination with
@@ -21,6 +26,7 @@ class RadiusGraph(BaseTransform):
     def __init__(
         self,
         r: float,
+        max_num_neighbors: int = 32,
         loop: bool = False,
         flow: str = 'source_to_target',
         num_workers: int = 1,
@@ -34,54 +40,58 @@ class RadiusGraph(BaseTransform):
     def forward(self, data: Data) -> Data:
         assert data.pos is not None
 
-        if data.lattice is not None and data.lattice.max(dim=-1).values.isfinite().any():
-            n_cell = (self.r / data.lattice.norm(dim=-1)).ceil().max(dim=0).values.int()
-            n_cell_tot = (2 * n_cell + 1).prod()
-            shift = torch.tensor([[i, j, k] for i in range(-n_cell[0], n_cell[0] + 1) for j in range(-n_cell[1], n_cell[1] + 1) for k in range(-n_cell[2], n_cell[2] + 1)], dtype=data.pos.dtype, device=data.pos.device)
-            shift = shift @ data.lattice
-            shift = shift.nan_to_num()
-            shifted_pos = data.pos[:, None, :] + shift  # shape: (n_node, n_cell_tot, 3)
-            shifted_pos = shifted_pos.reshape(-1, 3)  # shape: (n_node * n_cell_tot, 3)
-            shifted_node_index = torch.arange(data.pos.shape[0], dtype=torch.long, device=data.pos.device)[:, None].repeat(1, 27)  # shape: (n_node, n_cell_tot)
-            shifted_node_index = shifted_node_index.reshape(-1)  # shape: (n_node * n_cell_tot)
-            shifted_node_isoriginal = torch.zeros(data.pos.shape[0], 27, dtype=torch.bool, device=data.pos.device)  # shape: (n_node, n_cell_tot)
-            shifted_node_isoriginal[:, n_cell_tot // 2] = True
-            shifted_node_isoriginal = shifted_node_isoriginal.reshape(-1)  # shape: (n_node * n_cell_tot)
-            if data.batch is not None:
-                shifted_batch = data.batch[:, None].repeat(1, 27)  # shape: (n_node, n_cell_tot)
-                shifted_batch = shifted_batch.reshape(-1)  # shape: (n_node * n_cell_tot)
+        while True:
+            if data.lattice is not None and data.lattice.max(dim=-1).values.isfinite().any():
+                n_cell = (self.r / data.lattice.norm(dim=-1)).ceil().max(dim=0).values.int()
+                n_cell_tot = (2 * n_cell + 1).prod()
+                shift = torch.tensor([[i, j, k] for i in range(-n_cell[0], n_cell[0] + 1) for j in range(-n_cell[1], n_cell[1] + 1) for k in range(-n_cell[2], n_cell[2] + 1)], dtype=data.pos.dtype, device=data.pos.device)
+                shift = shift @ data.lattice
+                shift = shift.nan_to_num()
+                shifted_pos = data.pos[:, None, :] + shift  # shape: (n_node, n_cell_tot, 3)
+                shifted_pos = shifted_pos.reshape(-1, 3)  # shape: (n_node * n_cell_tot, 3)
+                shifted_node_index = torch.arange(data.pos.shape[0], dtype=torch.long, device=data.pos.device)[:, None].repeat(1, 27)  # shape: (n_node, n_cell_tot)
+                shifted_node_index = shifted_node_index.reshape(-1)  # shape: (n_node * n_cell_tot)
+                shifted_node_isoriginal = torch.zeros(data.pos.shape[0], 27, dtype=torch.bool, device=data.pos.device)  # shape: (n_node, n_cell_tot)
+                shifted_node_isoriginal[:, n_cell_tot // 2] = True
+                shifted_node_isoriginal = shifted_node_isoriginal.reshape(-1)  # shape: (n_node * n_cell_tot)
+                if data.batch is not None:
+                    shifted_batch = data.batch[:, None].repeat(1, 27)  # shape: (n_node, n_cell_tot)
+                    shifted_batch = shifted_batch.reshape(-1)  # shape: (n_node * n_cell_tot)
+                else:
+                    shifted_batch = None
+                shifted_edge_index = radius_graph(
+                    shifted_pos,
+                    self.r,
+                    shifted_batch,
+                    self.loop,
+                    max_num_neighbors=self.max_num_neighbors,
+                    flow=self.flow,
+                    num_workers=self.num_workers,
+                )#.sort(dim=0)[0].unique(dim=1)
+                shifted_edge_isoriginal = shifted_node_isoriginal[shifted_edge_index[0]]
+                shifted_edge_index = shifted_edge_index[:, shifted_edge_isoriginal]
+                data.edge_index = shifted_node_index[shifted_edge_index]
+                data.disp = shifted_pos[shifted_edge_index[0]] - shifted_pos[shifted_edge_index[1]]
             else:
-                shifted_batch = None
-            shifted_edge_index = radius_graph(
-                shifted_pos,
-                self.r,
-                shifted_batch,
-                self.loop,
-                max_num_neighbors=self.max_num_neighbors,
-                flow=self.flow,
-                num_workers=self.num_workers,
-            )#.sort(dim=0)[0].unique(dim=1)
-            shifted_edge_isoriginal = shifted_node_isoriginal[shifted_edge_index[0]]
-            shifted_edge_index = shifted_edge_index[:, shifted_edge_isoriginal]
-            data.edge_index = shifted_node_index[shifted_edge_index]
-            data.disp = shifted_pos[shifted_edge_index[0]] - shifted_pos[shifted_edge_index[1]]
-        else:
-            data.edge_index = radius_graph(
-                data.pos,
-                self.r,
-                data.batch,
-                self.loop,
-                max_num_neighbors=self.max_num_neighbors,
-                flow=self.flow,
-                num_workers=self.num_workers,
-            )#.sort(dim=0)[0].unique(dim=1)
-            data.disp = data.pos[data.edge_index[0]] - data.pos[data.edge_index[1]]
+                data.edge_index = radius_graph(
+                    data.pos,
+                    self.r,
+                    data.batch,
+                    self.loop,
+                    max_num_neighbors=self.max_num_neighbors,
+                    flow=self.flow,
+                    num_workers=self.num_workers,
+                )#.sort(dim=0)[0].unique(dim=1)
+                data.disp = data.pos[data.edge_index[0]] - data.pos[data.edge_index[1]]
 
-        # Keep the maximum number of neighbors sufficiently large
-        num_edges = data.edge_index.size(1)
-        max_num_neighbors = data.edge_index[0].bincount(minlength=data.pos.size(0)).max()
-        if (num_edges * 2 > self.max_num_neighbors * data.pos.size(0)) or (max_num_neighbors * 2 > self.max_num_neighbors):
-            self.max_num_neighbors *= 2
+            # Keep the maximum number of neighbors sufficiently large
+            max_num_neighbors = data.edge_index[0].bincount(minlength=data.pos.size(0)).max()
+            num_edges = data.edge_index.size(1)
+            if (self.max_num_neighbors > max_num_neighbors * 2) and (self.max_num_neighbors * data.pos.size(0) > num_edges * 2):
+                break
+            else:
+                self.max_num_neighbors *= 2
+                continue
 
         return data
 
